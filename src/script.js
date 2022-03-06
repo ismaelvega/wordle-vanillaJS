1001f0013fc79a61e7f9c20c7736f476050b825f
Ismael Vega <ismaelgrive@gmail.com>
2022-03-06 08:59:00 -0600

    Adds optional countdown and alerts when lost. Refactor for better code readibility

--- src/script.js
+++ src/script.js
@@ -1,16 +1,37 @@
-function checkUserInput(userInput){
+function checkUserInput({userInput, isLostInTime}){
+  const lastAttempt = currentRow === numberOfAttempts-1
+  const wrongWord = userInput !== currentWord
+  const outOfAttempts = lastAttempt && wrongWord
+
+  if(outOfAttempts){
+    console.log('jo');
+    lostAlert({outOfAttempts: true, isLostInTime: false})
+    userInput = currentWord
+  }
+
+  if(!outOfAttempts && !isLostInTime){
+    console.log('hey');
+    initiCountdown()
+  }
+  
   const rows = document.querySelectorAll('.grid_row')
   let checkingRow = rows[currentRow]
   let elementsFromCurrentRow = getElementsFromCurrentRow(checkingRow)
   fillRow(elementsFromCurrentRow, userInput)
 
   for(let i = 0; i < wordsLength; i++){
-    elementsFromCurrentRow[i].parentElement.style.background = '#787C7E'
-    if(currentWord.includes(userInput.charAt(i))){
-      elementsFromCurrentRow[i].parentElement.style.background = '#C9B458'
+    const letterToValidate = elementsFromCurrentRow[i].parentElement
+    const isWithinTheWordAndInTheSamePlace = userInput.charAt(i) === currentWord.charAt(i)
+    const isWithinTheWordButNotInTheSamePlace = currentWord.includes(userInput.charAt(i))
+    const isNotWithinTheWord = !isWithinTheWordAndInTheSamePlace && !isWithinTheWordButNotInTheSamePlace
+    if(isNotWithinTheWord){
+      letterToValidate.style.background = '#787C7E'
     }
-    if(userInput.charAt(i) === currentWord.charAt(i)){
-      elementsFromCurrentRow[i].parentElement.style.background = '#166916f0'
+    if(isWithinTheWordButNotInTheSamePlace){
+      letterToValidate.style.background = '#C9B458'
+    }
+    if(isWithinTheWordAndInTheSamePlace){
+      letterToValidate.style.background = '#166916f0'
       // return
     }
   }
@@ -26,6 +47,26 @@ function getElementsFromCurrentRow(row){
   return elementsArray
 }
 
+function initiCountdown(){
+  secondsPerAttempt = 45
+  window.countdown.textContent = String(secondsPerAttempt)
+  var countdownInterval = setInterval(() => {
+    // if(outOfAttempts){
+    //   clearInterval(countdownInterval)
+    // }
+    let timeIsOver = secondsPerAttempt === -1
+    if(timeIsOver){
+      lostAlert({userInput: currentWord, isLostInTime: true})
+      clearInterval(countdownInterval)
+    }
+    window.countdown.textContent = String(secondsPerAttempt--)
+  }, 1000);
+  if(currentRow !== 0){
+    clearInterval(countdownInterval)
+    clearInterval(countdownInterval)
+  }
+}
+
 function fillRow(lettersToReplace, submittedWord){
   lettersToReplace.forEach((letter, index) => {
     let letterToReplace = letter
@@ -34,6 +75,24 @@ function fillRow(lettersToReplace, submittedWord){
   })
 }
 
+function lostAlert({outOfAttempts, isLostInTime}){
+  alert(`sorry mate, the word was ${currentWord}`)
+  console.log(isLostInTime);
+  if(outOfAttempts){
+    window.countdown.style.display = 'none'
+  }
+
+  if(isLostInTime){
+    console.log('time')
+    console.log(secondsPerAttempt);
+    checkUserInput({userInput: currentWord})
+    secondsPerAttempt = 0
+  }
+  userInput.disabled = true
+  userInput.value = ''
+}
+
+
 
 // UTILS
 function getRandomInt(max) {
@@ -41,11 +100,13 @@ function getRandomInt(max) {
 }
 // STARTUP
 var wordsLength = 6
+var numberOfAttempts = 6
+var secondsPerAttempt = 45
 var words = [
   "cielos",
   "lengua",
   "Daniel",
-  "Romanos",
+  "Romano",
   "pecado",
   "Moises",
   "cielos",
@@ -59,13 +120,13 @@ var currentWord = words[getRandomInt(words.length)].toUpperCase()
 console.log(`Word: ${currentWord}`)
 var currentRow = 0
 
-let userInput = document.querySelector('#userInput')
+var userInput = document.querySelector('#userInput')
 userInput.addEventListener('keydown', (event) => {
   if(event.code === 'Enter'){
-    if(userInput.value.length < 6){
-      alert('6 letters')
+    if(userInput.value.length < wordsLength){
+      alert(`${wordsLength} letters`)
     } else {
-      checkUserInput(userInput.value.toUpperCase())
+      checkUserInput({userInput: userInput.value.toUpperCase()})
       userInput.value = ''
       currentRow++
     }
